bd8bf795d22dbaf7566779ac443cb07e21ae1aa5
EngrPips <95760918+EngrPips@users.noreply.github.com>
2023-03-23 14:55:11 -0700

    setting up the ico project folder
    setting up the ico project folder

---
+++ hardhat-environment/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x2Fb9BD236eE73cB88EBAc5d44E7a53B80107d9AC";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
